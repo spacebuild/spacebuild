6678fd23cc2cb16a204889995beaefbcc661d017
Stijn Vlaes <stijn.sv@gmail.com>
2012-12-23 00:57:46 +0100

    [Optimized] Player suit, independant code nog from ResourceContainer (to much possible overhead)

--- lua/sb/classes/PlayerSuit.lua
+++ lua/sb/classes/PlayerSuit.lua
@@ -15,13 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ]]
 
-include("sb/classes/ResourceContainer.lua")
-
--- Lua Specific
-local type = type
-
 -- Gmod specific
-local Entity = Entity
 local CurTime = CurTime
 local net = net
 
@@ -30,52 +24,91 @@ local C = CLASS
 local sb = sb;
 local core = sb.core
 
-local base_amount = 0
-
--- Function Refs
-local funcRef = {
-    isA = C.isA,
-    init = C.init,
-    addResource = C.addResource,
-    removeResource = C.removeResource,
-    supplyResource = C.supplyResource,
-    consumeResource = C.consumeResource,
-    getResourceAmount = C.getResourceAmount,
-    getMaxResourceAmount = C.getMaxResourceAmount,
-    sendContent = C._sendContent,
-    receiveSignal = C.receive
-}
-
 function C:init(ply)
-    funcRef.init(self, ply)
-    self:addResource("oxygen", 1000, base_amount)
-    self:addResource("coolant", 1000, base_amount)
-    self:addResource("energy", 1000, base_amount)
-    self:addResource("temperature", 9999, 0)
-    self:addResource("gravity", 9999, 0)
-end
-
-function C:send(modified, _)
-    if self.modified > modified then
-        net.Start("SBRPU")
-        self:_sendContent(modified)
-        net.Send(self.syncid)
-    end
+    self.ply = ply
+    self:reset()
 end
 
 function C:reset()
-    self:setResourceAmount("oxygen", base_amount)
-    self:setResourceAmount("coolant", base_amount)
-    self:setResourceAmount("energy", base_amount)
+    self.active = false
+    self.oxygen = 0
+    self.coolant = 0
+    self.energy = 0
+    self.temperature = 0
+    self.gravity = 0
+    self.modified = CurTime()
 end
 
-local res
+function C:setActive(active)
+   self.active = active
+end
 
-function C:setResourceAmount(name, amount)
-    if not self:containsResource(name) then error("Trying to set a value on a resource not available in the suit") end
-    res = self.resources[name]
-    res:setAmount(amount)
-    if self.modified < res:getModified() then
-        self.modified = res:getModified()
+function C:getActive()
+   return self.active
+end
+
+function C:setOxygen(oxygen)
+   self.oxygen = oxygen
+end
+
+function C:getOxygen()
+   return self.oxygen
+end
+
+function C:setCoolant(coolant)
+    self.coolant = coolant
+end
+
+function C:getCoolant()
+   return self.coolant
+end
+
+function C:setEnergy(energy)
+   self.energy = energy
+end
+
+function C:getEnergy()
+   return self.energy
+end
+
+function C:setTemperature(temperature)
+    self.temperature = temperature
+end
+
+function C:getTemperature()
+    return self.temperature
+end
+
+function C:setGravity(gravity)
+    self.gravity = gravity
+end
+
+function C:getGravity()
+    return self.gravity
+end
+
+function C:send(modified)
+    if self.modified > modified then
+        net.Start("SBRPU")
+        core.net.writeBool(self.active)
+        if self.active then
+            core.net.writeShort(self.oxygen)
+            core.net.writeShort(self.coolant)
+            core.net.writeShort(self.energy)
+            core.net.writeShort(self.temperature)
+            core.net.writeShort(self.gravity)
+        end
+        net.Send(self.ply)
+    end
+end
+
+function C:receive()
+    self.active = core.net.readBool()
+    if self.active then
+        self.oxygen = core.net.readShort()
+        self.coolant = core.net.readShort()
+        self.energy = core.net.readShort()
+        self.temperature = core.net.readShort()
+        self.gravity = core.net.readShort()
     end
 end
