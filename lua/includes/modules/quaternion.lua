0a912bf5bf6257c2240c8c63eb7170f6ebd2b3eb
Ashley Williamson <ashley@shortmail.me>
2013-01-04 23:08:20 +0000

    [Added] Quaternion module - Don't even mention anything wrong with it, VERY BETA [Added] Playersuit module for FluixHUD

---
+++ lua/includes/modules/quaternion.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Ash
+-- Date: 03/01/13
+-- Time: 17:24
+-- To change this template use File | Settings | File Templates.
+--
+
---
+++ lua/sb/extensions/fluixHUD/fluixmodules/playersuit.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Ash
+-- Date: 02/01/13
+-- Time: 02:53
+-- To change this template use File | Settings | File Templates.
+--
+
