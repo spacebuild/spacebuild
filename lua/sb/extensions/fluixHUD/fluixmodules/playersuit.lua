f4b4e692b683e6d8e40a4460d0f9089c38db878f
Stijn Vlaes <stijn.sv@gmail.com>
2013-01-29 19:59:37 +0100

    [Added] Debug player suit info

--- lua/sb/extensions/fluixHUD/fluixmodules/playersuit.lua
+++ lua/sb/extensions/fluixHUD/fluixmodules/playersuit.lua
@@ -27,7 +27,7 @@ else
 end
 
 
-local white, orange, red, green, percent = Color( 255, 255, 255, 240 ), Color( 255, 127, 36, 240 ), Color( 205, 51, 51, 240), Color(51, 205, 51, 240)
+local white, orange, red, green, bg, percent = Color( 255, 255, 255, 240 ), Color( 255, 127, 36, 240 ), Color( 205, 51, 51, 240), Color(51, 205, 51, 240), Color( 50,50,50,220)
 local function getColorBasedOnValue(component, value, maxvalue)
     percent = value/maxvalue
     if percent > 0.3 then
@@ -53,7 +53,7 @@ suitPanel = class.new("HudPanel", 16, 16,0, 0, false);
 --suitPanel:addChild(class.new("HudBarIndicator", 0, height * 2, width, height, "Energy: %i units", function() return suit:getEnergy() end, getColorBasedOnValue,  function() return 2000 end))
 --suitPanel:addChild(class.new("HudBarIndicator", 0, height * 3, width, height, "Temperature: %iK", function() return suit:getTemperature() end, getColorBasedOnTemperature,  function() return 1000 end))
 
-environmentPanel = class.new("HudPanel", scrW - (height + 16) , 16, false);
+environmentPanel = class.new("HudPanel", scrW - (width + 16) , 16, false);
 
 
 function fluix.modules.playerSuit.Run()
@@ -67,4 +67,23 @@ function fluix.modules.playerSuit.Run()
     if suit:getEnvironment() then
        environmentPanel:render()
     end
+    draw.RoundedBox( 1, 16, 16, 150, 100, bg )
+    draw.SimpleText( "Suit information", "HudHintTextSmall",  32, 32, white)
+    draw.SimpleText( string.format( "Oxygen: %i units", math.Round( suit:getOxygen() )), "HudHintTextSmall",  32, 48, white)
+    draw.SimpleText( string.format( "Coolant: %i units", math.Round( suit:getCoolant() )), "HudHintTextSmall",  32, 64, white)
+    draw.SimpleText( string.format( "Energy: %i units", math.Round( suit:getEnergy() )), "HudHintTextSmall",  32, 80, white)
+    draw.SimpleText( string.format( "Temperature: %iK", math.Round( suit:getTemperature() )), "HudHintTextSmall",  32, 96, white)
+
+    if suit:getEnvironment() then
+        local x = scrW - (200 + 16)
+        local env = suit:getEnvironment()
+        PrintTable(env)
+        draw.RoundedBox( 1, x, 16, 150, 100, bg )
+        draw.SimpleText( "Environment information", "HudHintTextSmall",  x + 16, 32, white)
+        draw.SimpleText( string.format( "Temperature: %i K", math.Round( env:getTemperature() )), "HudHintTextSmall",  x+16, 48, white)
+        draw.SimpleText( string.format( "Gravity: %2f",  env:getGravity() ), "HudHintTextSmall",  x+16, 64, white)
+        draw.SimpleText( string.format( "Atmosphere: %2f", env:getAtmosphere() ), "HudHintTextSmall",  x + 16, 80, white)
+        --draw.SimpleText( string.format( "Oxygen: %iK",  suit:getTemperature() ), "HudHintTextSmall",  x + 16, 96, white)
+    end
+
 end
