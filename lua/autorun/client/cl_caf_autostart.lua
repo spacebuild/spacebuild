e76e69e44f36a73da6ae7f22101201a5883f2967
X-Coder <gmod@x-coder.de>
2016-12-24 23:18:06 +0100

    Fixed upper cased folder names

--- lua/autorun/client/cl_caf_autostart.lua
+++ lua/autorun/client/cl_caf_autostart.lua
@@ -763,7 +763,7 @@ net.Receive("CAF_Addon_POPUP", ProccessMessage)
 
 --Core
 
-local Files = file.Find( "CAF/Core/client/*.lua" , "LUA")
+local Files = file.Find( "caf/core/client/*.lua" , "LUA")
 for k, File in ipairs(Files) do
 	Msg(CAF.GetLangVar("Loading")..": "..File.."...")
 	local ErrorCheck, PCallError = pcall(include, "caf/core/client/"..File)
@@ -774,7 +774,7 @@ for k, File in ipairs(Files) do
 	end
 end
 
-Files = file.Find("CAF/LanguageVars/*.lua", "LUA")
+Files = file.Find("caf/languagevars/*.lua", "LUA")
 for k, File in ipairs(Files) do
 	Msg(CAF.GetLangVar("Loading")..": "..File.."...")
 	local ErrorCheck, PCallError = pcall(include, "caf/languagevars/"..File)
@@ -786,7 +786,7 @@ for k, File in ipairs(Files) do
 end
 
 --Addons
-local Files = file.Find( "CAF/Addons/client/*.lua" , "LUA")
+local Files = file.Find( "caf/addons/client/*.lua" , "LUA")
 for k, File in ipairs(Files) do
 	Msg(CAF.GetLangVar("Loading")..": "..File.."...")
 	local ErrorCheck, PCallError = pcall(include, "caf/addons/client/"..File)
