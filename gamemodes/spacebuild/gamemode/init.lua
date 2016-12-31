32da990b137e98bbd8b4feb6024374c1c6d127b0
sgv <stijn.sv@gmail.com>
2016-12-31 15:37:57 +0100

    add spacebuild gamemode for workshop

---
+++ gamemodes/spacebuild/gamemode/init.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Stijn
+-- Date: 29/05/2016
+-- Time: 11:46
+-- To change this template use File | Settings | File Templates.
+--
+
+include( "shared.lua" )
+
